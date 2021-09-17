8c0af87eaee762c13be556bf1fd8e539f0c14df4
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-17 13:15:07 +0530

    feat: inputVal() definition

--- bdayPal.js
+++ bdayPal.js
@@ -1,5 +1,19 @@
 var dob = document.querySelector("#b-date");
 var calcBtn= document.querySelector(".calc-btn");
+var outputDiv= document.querySelector(".output-div");
 
-console.log(dob.value)
+
+calcBtn.addEventListener("click",clickHandler)
+
+
+function clickHandler(){
+const input = inputVal();
+console.log(input.dobVal)
+
+}
+
+function inputVal(){
+    return { dobVal : dob.value}
+}
+
 
