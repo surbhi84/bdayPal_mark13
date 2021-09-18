4f7b9ca2c45ac1ae62c5dc2ec3075329955866c7
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-19 00:59:09 +0530

    feat: checkPalindrome() created

--- bdayPal.js
+++ bdayPal.js
@@ -1,28 +1,46 @@
 var dob = document.querySelector("#b-date");
-var calcBtn= document.querySelector(".calc-btn");
-var outputDiv= document.querySelector(".output-div");
+var calcBtn = document.querySelector(".calc-btn");
+var outputDiv = document.querySelector(".output-div");
 
+calcBtn.addEventListener("click", clickHandler);
 
-calcBtn.addEventListener("click",clickHandler)
+function clickHandler() {
+  const dobVal = inputVal();
 
+  if (inputValidation(input)) {
+   
 
-function clickHandler(){
-const input = inputVal();
-
-if(inputValidation(input)){
-    console.log("success")
-}
-}
-
-function inputVal(){
-    return { dobVal : dob.value}
+    checkPalindrome(input);
+  } else {
+    errorAlert();
+  }
 }
 
-function inputValidation(input){
-    console.log(input.dobVal)
- if(input.dobVal==  "") {
-      outputDiv.innerText = "Please enter a Date.";
- }
- else{
-    return true; }
+function inputVal() {
+  return dob.value;
+}
+
+function inputValidation(input) {
+  if (input.dobVal == "") return false;
+  return true;
+}
+
+function inputForm(){
+const date = 
+}
+
+console.log(checkPalindrome("mom"));
+
+function checkPalindrome(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  console.log(reverse);
+  if (reverse == str) return true;
+  return false;
+}
+
+function errorAlert() {
+  outputDiv.innerText = "Please enter a Date.";
 }
